c84a04a9548fb2624d3112b96872b0aaac42e503
rodrigo_mch <rodrigo.morais@estudante@ufjf.br>
2023-11-29 19:54:21 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// main.js
+
+//passo 1 - ache os elementos importantes
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#saida');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
